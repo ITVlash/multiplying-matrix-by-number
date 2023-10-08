db4d4a3e1787d75a31e3f70b14dce3ec97959c9e
vladi <v16012003@gmail.com>
2023-10-08 19:14:45 +0300

    Функция исправлена

--- src/operation.js
+++ src/operation.js
@@ -3,7 +3,7 @@ class Operation {
     multiplication_number(a, num) {
         for (let i = 0; i < a.length; i++) {
             for (let j = 0; j < a[0].length; j++) {
-                a[i][j] = a[i][j] * Math.trunc(num);
+                a[i][j] *= num;
             }
           }
           return a;
